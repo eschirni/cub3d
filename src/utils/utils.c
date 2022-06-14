474361300c25d1393bd6ebab203fd38d2391fb6c
Ben Tenzlinger <btenzlin@1-D-25.42heilbronn.de>
2022-06-14 16:12:50 +0200

    added functions

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -3,18 +3,64 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eschirni <eschirni@student.42.fr>          +#+  +:+       +#+        */
+/*   By: btenzlin <btenzlin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 21:57:53 by eschirni          #+#    #+#             */
-/*   Updated: 2022/06/13 23:50:41 by eschirni         ###   ########.fr       */
+/*   Updated: 2022/06/14 11:29:32 by btenzlin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
+
+static int	ft_strncmp(const char *str1, const char *str2, size_t size)
+{
+	unsigned char	*s1;
+	unsigned char	*s2;
+
+	s1 = (unsigned char *)str1;
+	s2 = (unsigned char *)str2;
+	while (size > 0 && (*s1 != '\0' || *s2 != '\0'))
+	{
+		if (*s1 == *s2)
+		{
+			s1++;
+			s2++;
+			size--;
+		}
+		else
+		{
+			return (*s1 - *s2);
+		}
+	}
+	return (0);
+}
+
+int	ft_strlen(char *s)
+{
+	int	i;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
 
 bool	is_char_obj(char c)
 {
 	if (c == 'N' || c == 'E' || c == 'S' || c == 'W')
 		return (true);
 	return (false);
+}
+
+int	check_file(char *file)
+{
+	char	*tmp;
+
+	tmp = file;
+	tmp += ft_strlen(file) - 4;
+	if (ft_strncmp(tmp, ".cub", 5))
+		return (0);
+	return (1);
 }
