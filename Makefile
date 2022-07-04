04fc8ddb004a7270e5335d4c89a87b9e7bf93e43
Ben Tenzlinger <btenzlin@1-D-25.42heilbronn.de>
2022-07-04 18:35:59 +0200

    merge fix

--- Makefile
+++ Makefile
@@ -6,12 +6,12 @@
 #    By: btenzlin <btenzlin@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/06/10 15:43:54 by eschirni          #+#    #+#              #
-#    Updated: 2022/07/04 18:02:55 by btenzlin         ###   ########.fr        #
+#    Updated: 2022/07/04 18:34:19 by btenzlin         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 # FLAGS
-CFLAGS = -Wall -Wextra -Werror
+CFLAGS = -Wall -Wextra -Werror -O3
 MLX = -lglfw -L $$HOME/.brew/Cellar/glfw/3.3.7/lib MLX42/libmlx42.a
 
 # COLORS
@@ -36,15 +36,16 @@ MAPGEN_PATH = ./src/map_gen/
 # SOURCES
 SRC =	$(SRC_PATH)main.c $(SRC_PATH)init_map.c \
 		$(UTILS_PATH)ft_error.c $(UTILS_PATH)utils.c \
-		$(UTILS_PATH)free_exit.c \
+		$(UTILS_PATH)free_exit.c $(UTILS_PATH)hooks_utils.c \
 		$(SRC_PATH)char_direction.c $(SRC_PATH)hooks.c \
-		$(SRC_PATH)ray.c \
+		$(SRC_PATH)ray.c $(SRC_PATH)generate_map.c \
 		$(DRAW_PATH)draw_game.c $(DRAW_PATH)draw_crosshair.c \
 		$(DRAW_PATH)draw_map.c $(DRAW_PATH)draw.c \
-		$(DRAW_PATH)draw_chars.c \
+		$(DRAW_PATH)draw_chars.c $(DRAW_PATH)draw_3d.c \
 		$(GNL_PATH)get_next_line.c $(GNL_PATH)get_next_line_utils.c \
 		$(MAIN_MENU_PATH)main_menu.c $(MAIN_MENU_PATH)hooks.c \
-		$(MAIN_MENU_PATH)settings.c $(MAPGEN_PATH)generate_map.c \
+		$(MAIN_MENU_PATH)settings.c $(MAIN_MENU_PATH)animations.c \
+		$(MAIN_MENU_PATH)switch_menu.c $(MAPGEN_PATH)generate_map.c \
 		$(MAPGEN_PATH)mapgen_utils.c \
 
 # OBJECTS
@@ -58,6 +59,12 @@ all: $(NAME)
 	@echo " _|         _|    _|   _|    _|     _|_|     _|    _|"
 	@echo " _|         _|    _|   _|    _|         _|   _|    _|"
 	@echo "   _|_|_|     _|_|_|   _|_|_|     _|_|_|     _|_|_|  "
+	#@osascript -e "set volume 3" && say "cub3d. ooooooooooh- yeah" -r 120  &
+	#@osascript -e "set volume 3" && say "cub3d. ooooooooooh- yeah" -r 120 -a 0 &
+	#@osascript -e "set volume 3" && say "cub3d. ooooooooooh" -r 120 -a 0 &
+	#@osascript -e "set volume 3" && say "cub3d. ooooooooooh" -r 120 -a 0 &
+	#@osascript -e "set volume 3" && say "cub3d. ooooooooooh" -r 120 -a 0 &
+	#@osascript -e "set volume 3" && say "cub3d. ooooooooooh" -r 120 -a 0 &
 	@echo $(X)
 
 $(OBJ_PATH)%.o :$(SRC_PATH)%.c
--- src/cub3d.h
+++ src/cub3d.h
@@ -6,24 +6,23 @@
 /*   By: btenzlin <btenzlin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 12:04:01 by btenzlin          #+#    #+#             */
-/*   Updated: 2022/07/04 18:22:11 by btenzlin         ###   ########.fr       */
+/*   Updated: 2022/07/04 18:35:31 by btenzlin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//todo: one mlx_loop only, draw live instead of creating a new image every second
+//todo: draw live instead of creating a new image every second
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# include "../MLX42/include/MLX42/MLX42.h"
-# include "../get_next_line/get_next_line.h"
+# include "../../MLX42/include/MLX42/MLX42.h"
+# include "../../get_next_line/get_next_line.h"
 # include <stdlib.h>
 # include <unistd.h>
 # include <limits.h>
 # include <fcntl.h>
 # include <math.h>
 # include <stdio.h>	//debug
-# include <sys/time.h>
 
 # define RED "\033[31m"
 # define RESET "\033[0m"
@@ -50,26 +49,51 @@ typedef struct s_char
 	float		s[2];
 	float		d[2];
 	float		pa;
+	float		x;
+	float		y;
 }				t_char;
 
 typedef struct s_map
 {
-	char		**map_arr;
+	char		**big_map;
 	float		player[2];
 	int			x;
 	int			y;
+	int			n_chars;
+	mlx_image_t	*floor;
+	mlx_image_t	*out;
+	mlx_image_t	*wall;
 }				t_map;
 
 typedef struct s_menu
 {
 	bool		in_menu;
-	bool		in_settings;
+	char		scroll_mode;
 	int			back_frame;
 	int			scroll_frame;
-	mlx_image_t	*imgs[33];
 	long		back_seconds;
 	long		scroll_seconds;
+	mlx_image_t	*imgs[33];
 }				t_menu;
+
+typedef struct s_settings
+{
+	char		cross_type;
+	int			fov;
+	int			graphics;
+	float		rs;
+}				t_settings;
+
+typedef struct s_game
+{
+	t_char		**chars;
+	t_map		*map;
+	t_menu		*menu;
+	t_settings	*settings;
+	mlx_t		*mlx;
+	mlx_image_t	*game_img;
+	int			ps;
+}				t_game;
 
 typedef struct s_mapgen
 {
@@ -77,60 +101,49 @@ typedef struct s_mapgen
 	int			rand_dir[2];
 	int			last_dir[2];
 	int			start[2];
-	int			player_start[2];
-	int			rand;
-	int			size;
-	char		**map;
 }				t_mapgen;
 
-typedef struct s_game
-{
-	t_char		**chars;
-	t_map		*map;
-	t_menu		*menu;
-	mlx_t		*mlx;
-	mlx_image_t	*game_img;
-	mlx_image_t	*floor;
-	mlx_image_t	*out;
-	mlx_image_t	*wall;
-	int			n_chars;
-}				t_game;
-
 /* map parsing */
-t_map		*init_map(char *mapfile);
+t_map	*init_map(char *mapfile);
 
 /* char fuctions */
-void		set_direction(t_game *game, char direction, int n);
+void	set_direction(t_game *game, char direction, int n);
 
 /* utils */
-bool		is_char_obj(char c);
-void		free_exit(t_map *map, t_game *game, t_mapgen *mapg);
-void		ft_error(char *msg, char *arg);
-int			ft_strlen(char *s);
-int			check_file(char *file);
-void		free_2d_array(char **arr);
+bool	is_char_obj(char c);
+void	free_exit(t_map *map, t_game *game);
+void	ft_error(char *msg, char *arg);
+int		ft_strlen(char *s);
+int		check_file(char *file);
+void	free_2d_array(char **arr);
 
 /* draw functions */
-void		draw_chars(t_game *game, char **map);
-void		draw_game(t_ray *ray, t_game *game, int x, int y);
-void		draw_map(t_game *game, t_map *map, float player[2]);
-void		draw_line(t_ray *ray, mlx_image_t *img, int color);
-void		draw_crosshair(mlx_t *mlx, int color);
+void	draw_3d(t_game *game, t_ray *ray, int count_x, int *line_x);
+void	draw_chars(t_game *game, char **map);
+void	draw_game(t_ray *ray, t_game *game, float coords[2]);
+void	draw_map(t_game *game, t_map *map);
+void	draw_line(t_ray *ray, mlx_image_t *img, int color);
+void	draw_crosshair(t_game *game, int color, char type);
+void	get_map_textures(t_game *game);
 
 /* hooks */
-void		calc_rotate(t_game *game, float rotation, int n);
-void		hook(void *tmp);
+void	calc_rotate(t_game *game, float rotation, int n);
+void	fps(void *tmp);
+void	move_map(t_game *game, int addX, int addY);
 
 /* rays */
-float		calc_rays(t_ray *ray, t_map *map, int x, int y);
+float	calc_rays(t_ray *ray, t_map *map, int x, int y);
 
 /* main menu */
+void	animate_menu(t_menu *menu);
 void	hover_buttons(double x, double y, void *tmp);
-void		main_menu(t_game *game);
+void	main_menu(t_game *game);
+void	to_game(t_game *game);
+void	to_menu(t_game *game);
 
 /* map gen */
-t_mapgen	*create_map(int size, int tunnels, int tunnel_len, int end_len);
-int			get_random_num(int from, int to);
-void		refactor_map(t_mapgen *mapg);
+char	**create_map(int size, int tunnels, int tunnel_len);
+int		get_random_num(int from, int to);
+void	refactor_map(t_mapgen *mapg);
 
 #endif
