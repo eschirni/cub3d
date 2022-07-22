f2d7a208b73ae48ba3a759994c8e9205e9cf3272
Ben Tenzlinger <btenzlin@1-D-25.42heilbronn.de>
2022-07-22 18:08:17 +0200

    switched to liam wynn function

--- src/draw_functions/draw_sprites.c
+++ src/draw_functions/draw_sprites.c
@@ -58,42 +58,46 @@
 // 	}
 // }
 
-// void	draw_sprite(t_game *game)
-// {
-// 	float	s_pos[2];
-// 	float	s_vec[2];
-// 	float	screen_pos[2];
-// 	int	angle_x;
-// 	float	fov_left;
+void	draw_sprite(t_game *game)
+{
+	float	s_pos[2];
+	float	s_vec[2];
+	float	screen_pos[2];
+	float	angle_x;
+	float	fov_left;
 
-// 	s_pos[0] = 250;
-// 	s_pos[1] = 340;
-// 	s_vec[0] = s_pos[0] - game->map->player[0];
-// 	s_vec[1] = s_pos[1] - game->map->player[1];
-// 	angle_x = (int)atan2f(-s_vec[1], s_vec[0]);
-// 	if (angle_x < 0)
-// 		angle_x += (float)M_PI * 2;
-// 	else if (angle_x >= ((float)M_PI * 2))
-// 		angle_x -= (float)M_PI * 2;
-// 	fov_left = game->chars[0]->pa + (game->menu->settings->fov / 2) - angle_x;
-// 	if ((game->chars[0]->pa >= 0 && game->chars[0]->pa < ((float)M_PI * 2)) && fov_left >= (2 * (float)M_PI / 3))
-// 		fov_left += (float)M_PI * 2;
-// 	else if ((fov_left >= 0 && fov_left < ((float)M_PI * 2)) && game->chars[0]->pa >= (2 * (float)M_PI / 3))
-// 		fov_left -= (float)M_PI * 2;
-// 	screen_pos[0] = fov_left * (WIDTH / game->menu->settings->fov);
-// 	screen_pos[1] = HEIGHT / 2;
-// 	// printf("%f, %f\n", screen_pos[0], screen_pos[1]);
-// 	if (screen_pos[0] < WIDTH && screen_pos[1] < HEIGHT && screen_pos[0] > 1 && screen_pos[1] > 1)
-// 	{
-// 		mlx_put_pixel(game->game_img, screen_pos[0], screen_pos[1], 0xFF0000FF);
-// 		mlx_put_pixel(game->game_img, screen_pos[0] - 1, screen_pos[1] - 1, 0xFF0000FF);
-// 		mlx_put_pixel(game->game_img, screen_pos[0] - 1, screen_pos[1], 0xFF0000FF);
-// 		mlx_put_pixel(game->game_img, screen_pos[0], screen_pos[1] + 1, 0xFF0000FF);
-// 		mlx_put_pixel(game->game_img, screen_pos[0] + 1, screen_pos[1] + 1, 0xFF0000FF);
-// 		mlx_put_pixel(game->game_img, screen_pos[0] + 1, screen_pos[1], 0xFF0000FF);
-// 		mlx_put_pixel(game->game_img, screen_pos[0], screen_pos[1] + 1, 0xFF0000FF);
-// 	}
-// }
+	s_pos[0] = 48;
+	s_pos[1] = 48;
+	s_vec[0] = s_pos[0] - game->map->player[0];
+	s_vec[1] = s_pos[1] - game->map->player[1];
+	angle_x = atan2f(-s_vec[1], s_vec[0]);
+	if (angle_x < 0)
+		angle_x += (float)M_PI * 2;
+	else if (angle_x >= (float)M_PI * 2)
+		angle_x -= (float)M_PI * 2;
+	fov_left = game->chars[0]->pa + (game->menu->settings->graphics / 2) - angle_x;
+	if ((game->chars[0]->pa >= 0 && game->chars[0]->pa < ((float)M_PI * 2)) && fov_left >= (2 * (float)M_PI / 3))
+		fov_left += (float)M_PI * 2;
+	else if ((fov_left >= 0 && fov_left < ((float)M_PI * 2)) && game->chars[0]->pa >= (2 * (float)M_PI / 3))
+		fov_left -= (float)M_PI * 2;
+	screen_pos[0] = fov_left * (WIDTH / game->menu->settings->graphics);
+	screen_pos[1] = HEIGHT / 2;
+	// printf("%f, %f\n", screen_pos[0], screen_pos[1]);
+	int	i = 0;
+	int	j = 0;
+	while (i < 100)
+	{
+		j = 0;
+		while (j < 100)
+		{
+			if (screen_pos[0] - i < WIDTH && screen_pos[1] - j < HEIGHT && screen_pos[0] - i > 1 && screen_pos[1] - j > 1)
+				mlx_put_pixel(game->game_img, screen_pos[0] - i, screen_pos[1] - j, 0xFF0000FF);
+			j++;
+		}
+		i++;
+	} 
+}
+
 
 // void	draw_sprite(t_game *game)
 // {
@@ -102,60 +106,67 @@
 // 	float	screen_pos[2];
 // 	int		angle_x;
 
-// 	s_pos[0] = 128;
-// 	s_pos[1] = 128;
+// 	s_pos[0] = 32 + 16;
+// 	s_pos[1] = 32 + 16;
 // 	s_vec[0] = s_pos[0] - game->map->player[0];
 // 	s_vec[1] = s_pos[1] - game->map->player[1];
-// 	angle_x = (int)atan2f(-s_vec[1], s_vec[0]);
+// 	angle_x = (int)atan2f(s_vec[1], s_vec[0]);
 // 	if (angle_x < 0)
 // 		angle_x += (float)M_PI * 2;
 // 	screen_pos[1] = game->chars[0]->pa + (game->menu->settings->graphics / 2) - angle_x;
 // 	if (angle_x > ((float)M_PI * 2 / 3) && game->chars[0]->pa < (float)(M_PI / 2))
 // 		screen_pos[1] = game->chars[0]->pa + (game->menu->settings->graphics / 2) - angle_x + ((float)M_PI * 2);
-// 	if (game->chars[0]->pa > ((float)M_PI * 2 / 3) && angle_x < (float)(M_PI / 2))
+// 	else if (game->chars[0]->pa > ((float)M_PI * 2 / 3) && angle_x < (float)(M_PI / 2))
 // 		screen_pos[1] = game->chars[0]->pa + (game->menu->settings->graphics / 2) - angle_x - ((float)M_PI * 2);
 // 	screen_pos[0] = screen_pos[1] * WIDTH / game->menu->settings->graphics;
-// 	// printf("%f, %f\n", screen_pos[0], screen_pos[1]);
-// 	if (screen_pos[0] < WIDTH && screen_pos[1] < HEIGHT && screen_pos[0] > 1 && screen_pos[1] > 1)
+// 	printf("%f, %f\n", screen_pos[0], screen_pos[1]);
+// 	int	i = 0;
+// 	int	j = 0;
+// 	while (i < 100)
 // 	{
-// 		mlx_put_pixel(game->game_img, screen_pos[0], screen_pos[1], 0xFF0000FF);
-// 		mlx_put_pixel(game->game_img, screen_pos[0] - 1, screen_pos[1] - 1, 0xFF0000FF);
-// 		mlx_put_pixel(game->game_img, screen_pos[0] - 1, screen_pos[1], 0xFF0000FF);
-// 		mlx_put_pixel(game->game_img, screen_pos[0], screen_pos[1] - 1, 0xFF0000FF);
-// 	}
+// 		j = 0;
+// 		while (j < 100)
+// 		{
+// 			if (screen_pos[0] - i < WIDTH && screen_pos[1] - j < HEIGHT && screen_pos[0] - i > 1 && screen_pos[1] - j > 1)
+// 				mlx_put_pixel(game->game_img, screen_pos[0] - i, HEIGHT / 2 - j, 0xFF0000FF);
+// 			j++;
+// 		}
+// 		i++;
+// 	} 
 // }
 
-void	draw_sprite(t_game *game)
-{
-	float	screen_pos[3];
-	float	sp_pos[3];
+// void	draw_sprite(t_game *game)
+// {
+// 	float	screen_pos[3];
+// 	float	sp_pos[3];
 
-	sp_pos[0] = 32 + 16;
-	sp_pos[1] = 32 + 16;
-	sp_pos[2] = HEIGHT / 4;
-	screen_pos[0] = sp_pos[0] - game->map->player[0];
-	screen_pos[1] = sp_pos[1] - game->map->player[1];
-	screen_pos[2] = sp_pos[2];
-	float	a = screen_pos[1] * cos(game->chars[0]->pa) + screen_pos[0] * sin(game->chars[0]->pa);
-	float	b = screen_pos[0] * cos(game->chars[0]->pa) + screen_pos[1] * sin(game->chars[0]->pa);
-	screen_pos[0] = a;
-	screen_pos[1] = b;
-	screen_pos[0] = (screen_pos[0] / screen_pos[1]) + (WIDTH / 2);
-	screen_pos[1] = (screen_pos[2] * 60 / screen_pos[1]) + (HEIGHT / 2);
-	// printf("%f, %f\n", game->map->player[0], game->map->player[1]);
-	int	i = 0;
-	int	j = 0;
-	while (i < 100)
-	{
-		j = 0;
-		while (j < 100)
-		{
-			if (screen_pos[0] - i < WIDTH && screen_pos[1] - j < HEIGHT && screen_pos[0] - i > 1 && screen_pos[1] - j > (HEIGHT / 2) && b < game->rays[(int)screen_pos[0] - i])
-				mlx_put_pixel(game->game_img, screen_pos[0] - i, screen_pos[1] - j, 0xFF0000FF);
-			j++;
-		}
-		i++;
-	} 
-}
+// 	sp_pos[0] = 32 + 16;
+// 	sp_pos[1] = 32 + 16;
+// 	sp_pos[2] = HEIGHT / 4;
+// 	screen_pos[0] = sp_pos[0] - game->map->player[0];
+// 	screen_pos[1] = sp_pos[1] - game->map->player[1];
+// 	screen_pos[2] = sp_pos[2];
+// 	printf("%f\n", game->chars[0]->pa);
+// 	float	a = screen_pos[1] * cos(game->chars[0]->pa) + screen_pos[0] * sin(game->chars[0]->pa);
+// 	float	b = screen_pos[0] * cos(game->chars[0]->pa) + screen_pos[1] * sin(game->chars[0]->pa);
+// 	screen_pos[0] = a;
+// 	screen_pos[1] = b;
+// 	screen_pos[0] = (screen_pos[0] / screen_pos[1]) + (WIDTH / 2);
+// 	screen_pos[1] = (screen_pos[2] * 60 / screen_pos[1]) + (HEIGHT / 2);
+// 	// printf("%f, %f\n", game->map->player[0], game->map->player[1]);
+// 	int	i = 0;
+// 	int	j = 0;
+// 	while (i < 100)
+// 	{
+// 		j = 0;
+// 		while (j < 100)
+// 		{
+// 			if (screen_pos[0] - i < WIDTH && screen_pos[1] - j < HEIGHT && screen_pos[0] - i > 1 && screen_pos[1] - j > 1 && b < game->rays[(int)screen_pos[0] - i])
+// 				mlx_put_pixel(game->game_img, screen_pos[0] - i, screen_pos[1] - j, 0xFF0000FF);
+// 			j++;
+// 		}
+// 		i++;
+// 	} 
+// }
 
 //x behaves like an asshole + block hiding behind wall calc is off (maybe because of out inefficient direction calculation)
